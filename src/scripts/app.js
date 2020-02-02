c76b1bba08edd36eb8520489aba45154c8c500ee
amrh20 <amr.hamdy@businessboomers.net>
2020-02-02 17:29:54 +0200

    styling product details

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
-    // show cart content at header
-    $(".cart-icon_head").click(function () {
-        $(".shopping-cart_content").toggleClass("showcontent");
-        // if ($(".wishlist-view_content").hasClass("showcontent")) {
-        //     $(".wishlist-view_content").removeClass("showcontent")
-        // }
-        // if ($(".compare-view_content").hasClass("showcontent")) {
-        //     $(".compare-view_content").removeClass("showcontent")
+	// show cart content at header
+	$(".cart-icon_head").click(function () {
+		$(".shopping-cart_content").toggleClass("showcontent");
+		// if ($(".wishlist-view_content").hasClass("showcontent")) {
+		//     $(".wishlist-view_content").removeClass("showcontent")
+		// }
+		// if ($(".compare-view_content").hasClass("showcontent")) {
+		//     $(".compare-view_content").removeClass("showcontent")
 		// }
 		$(".cart-icon_head i").toggleClass("whiteColor");
 		if ($(".wishlist-icon_head i").hasClass("whiteColor")) {
 			$(".wishlist-icon_head i").removeClass("whiteColor")
-        }
-        if ($(".compare-icon_head i").hasClass("whiteColor")) {
+		}
+		if ($(".compare-icon_head i").hasClass("whiteColor")) {
 			$(".compare-icon_head i").removeClass("whiteColor")
-		}	
+		}
 		$(".wishlist-view").toggleClass("hideIcons")
 		$(".compare-view").toggleClass("hideIcons")
 		$(".account-view").toggleClass("hideIcons")
-	//   show overlay
-	$(".body_overlay").toggleClass("show_overlay")
-    })
-    // show wishlist content at header
-    $(".wishlist-icon_head").click(function () {
-        $(".wishlist-view_content").toggleClass("showcontent")
-        // if ($(".shopping-cart_content").hasClass("showcontent")) {
-        //     $(".shopping-cart_content").removeClass("showcontent")
-        // }
-        // if ($(".wishlist-view_content").hasClass("showcontent")) {
-        //     $(".compare-view_content").removeClass("showcontent")
+		//   show overlay
+		$(".body_overlay").toggleClass("show_overlay")
+	})
+	// show wishlist content at header
+	$(".wishlist-icon_head").click(function () {
+		$(".wishlist-view_content").toggleClass("showcontent")
+		// if ($(".shopping-cart_content").hasClass("showcontent")) {
+		//     $(".shopping-cart_content").removeClass("showcontent")
+		// }
+		// if ($(".wishlist-view_content").hasClass("showcontent")) {
+		//     $(".compare-view_content").removeClass("showcontent")
 		// }
 		$(".shopping-cart").toggleClass("hideIcons")
 		$(".compare-view").toggleClass("hideIcons")
@@ -37,21 +37,21 @@ $(document).ready(function () {
 		$(".wishlist-icon_head i").toggleClass("whiteColor")
 		if ($(".cart-icon_head i").hasClass("whiteColor")) {
 			$(".cart-icon_head i").removeClass("whiteColor")
-        }
-        if ($(".compare-icon_head i").hasClass("whiteColor")) {
+		}
+		if ($(".compare-icon_head i").hasClass("whiteColor")) {
 			$(".compare-icon_head i").removeClass("whiteColor")
 		}
 		//   show overlay
-	$(".body_overlay").toggleClass("show_overlay")
-    })
-    // show compare content at header
-    $(".compare-icon_head").click(function () {
-        $(".compare-view_content").toggleClass("showcontent");
-        // if ($(".wishlist-view_content").hasClass("showcontent")) {
-        //     $(".wishlist-view_content").removeClass("showcontent")
-        // }
-        // if ($(".shopping-cart_content").hasClass("showcontent")) {
-        //     $(".shopping-cart_content").removeClass("showcontent")
+		$(".body_overlay").toggleClass("show_overlay")
+	})
+	// show compare content at header
+	$(".compare-icon_head").click(function () {
+		$(".compare-view_content").toggleClass("showcontent");
+		// if ($(".wishlist-view_content").hasClass("showcontent")) {
+		//     $(".wishlist-view_content").removeClass("showcontent")
+		// }
+		// if ($(".shopping-cart_content").hasClass("showcontent")) {
+		//     $(".shopping-cart_content").removeClass("showcontent")
 		// }
 		$(".shopping-cart").toggleClass("hideIcons")
 		$(".wishlist-view").toggleClass("hideIcons")
@@ -62,34 +62,34 @@ $(document).ready(function () {
 			$(".cart-icon_head i").removeClass("whiteColor")
 		}
 		if ($(".wishlist-icon_head i").hasClass("whiteColor")) {
-            $(".wishlist-icon_head i").removeClass("whiteColor")
+			$(".wishlist-icon_head i").removeClass("whiteColor")
 		}
 		//   show overlay
-	$(".body_overlay").toggleClass("show_overlay")
-    })
-    // show account view at header
-    $(".account-icon_head").click(function () {
-        $(".accounts_view ").css("right", "0");
-        if ($(".wishlist-view_content").hasClass("showcontent")) {
-            $(".wishlist-view_content").removeClass("showcontent")
-        }
-        if ($(".compare-view_content").hasClass("showcontent")) {
-            $(".compare-view_content").removeClass("showcontent")
-        }
-        if ($(".shopping-cart_content").hasClass("showcontent")) {
-            $(".shopping-cart_content").removeClass("showcontent")
+		$(".body_overlay").toggleClass("show_overlay")
+	})
+	// show account view at header
+	$(".account-icon_head").click(function () {
+		$(".accounts_view ").css("right", "0");
+		if ($(".wishlist-view_content").hasClass("showcontent")) {
+			$(".wishlist-view_content").removeClass("showcontent")
+		}
+		if ($(".compare-view_content").hasClass("showcontent")) {
+			$(".compare-view_content").removeClass("showcontent")
+		}
+		if ($(".shopping-cart_content").hasClass("showcontent")) {
+			$(".shopping-cart_content").removeClass("showcontent")
 		}
 		//   show overlay
-	$(".body_overlay").toggleClass("show_overlay")
-    })
-    // close account view
-    $(".close_accountView").click(function () {
+		$(".body_overlay").toggleClass("show_overlay")
+	})
+	// close account view
+	$(".close_accountView").click(function () {
 		$(".accounts_view ").css("right", "-350px");
 		$(".body_overlay").removeClass("show_overlay");
-	
-    })
+
+	})
 	//  close all dropdowns on click out side
-	$(".body_overlay").click(function(){
+	$(".body_overlay").click(function () {
 		$(".custom_content").removeClass("showcontent")
 		$(".body_overlay").removeClass("show_overlay")
 		$(".wishlist-view").removeClass("hideIcons")
@@ -97,30 +97,30 @@ $(document).ready(function () {
 		$(".account-view").removeClass("hideIcons")
 		$(".shopping-cart").removeClass("hideIcons")
 		$(".header .mid_header .mid_header-content .icons i").removeClass("whiteColor")
-		$(".accounts_view").css("right","-350px")
+		$(".accounts_view").css("right", "-350px")
 	})
 	// side side menu  
-	$(".menu-btn").click(function(){
+	$(".menu-btn").click(function () {
 		$(".side_menu").toggleClass("view_side-menu")
 		$(".body_overlay").addClass("show_overlay");
 		$(".wrapper_content").toggleClass("act")
 		$(this).toggleClass("opactiy_zero")
 	})
 	// close side menu
-	$(".body_overlay").click(function(){
+	$(".body_overlay").click(function () {
 		$(".side_menu").removeClass("view_side-menu");
 		$(".wrapper_content").removeClass("act");
 		$(".menu-btn").removeClass("opactiy_zero")
 		$(".childern").removeClass("showchildern");
 	})
-	$(".close_sidemenu").click(function(){
+	$(".close_sidemenu").click(function () {
 		$(".side_menu").removeClass("view_side-menu");
 		$(".wrapper_content").removeClass("act");
 		$(".menu-btn").toggleClass("opactiy_zero");
 		$(".body_overlay").removeClass("show_overlay");
 	})
 	// show childern at side menu
-	$("ul.categories li i").click(function(){
+	$("ul.categories li i").click(function () {
 		console.log("test")
 		$("div").toggleClass("showchildern")
 	})
@@ -128,196 +128,196 @@ $(document).ready(function () {
 
 // main slider
 
-	const $cont = $('.cont');
-	const $slider = $('.slider');
-	const $nav = $('.nav');
-	const winW = $(window).width();
-	const animSpd = 750; // Change also in CSS
-	const distOfLetGo = winW * 0.2;
-	let curSlide = 1;
-	let animation = false;
-	let autoScrollVar = true;
-	let diff = 0;
-	
-	// Generating slides
-	let arrCities = ['Amsterdam', 'Rome', 'New—York', 'Singapore', 'Prague']; // Change number of slides in CSS also
-	let numOfCities = arrCities.length;
-	let arrCitiesDivided = [];
+const $cont = $('.cont');
+const $slider = $('.slider');
+const $nav = $('.nav');
+const winW = $(window).width();
+const animSpd = 750; // Change also in CSS
+const distOfLetGo = winW * 0.2;
+let curSlide = 1;
+let animation = false;
+let autoScrollVar = true;
+let diff = 0;
 
-	arrCities.map((city) => {
-		let length = city.length;
-		let letters = Math.floor(length / 4);
-		let exp = new RegExp(".{1," + letters + "}", "g");
-		
-		arrCitiesDivided.push(city.match(exp));
-	});
-	
-	let generateSlide = function(city) {
-		let frag1 = $(document.createDocumentFragment());
-		let frag2 = $(document.createDocumentFragment());
-		const numSlide = arrCities.indexOf(arrCities[city]) + 1;
-		const firstLetter = arrCitiesDivided[city][0].charAt(0);
+// Generating slides
+let arrCities = ['Amsterdam', 'Rome', 'New—York', 'Singapore', 'Prague']; // Change number of slides in CSS also
+let numOfCities = arrCities.length;
+let arrCitiesDivided = [];
 
-		const $slide =
-					$(`<div data-target="${numSlide}" class="slide slide--${numSlide}">
+arrCities.map((city) => {
+	let length = city.length;
+	let letters = Math.floor(length / 4);
+	let exp = new RegExp(".{1," + letters + "}", "g");
+
+	arrCitiesDivided.push(city.match(exp));
+});
+
+let generateSlide = function (city) {
+	let frag1 = $(document.createDocumentFragment());
+	let frag2 = $(document.createDocumentFragment());
+	const numSlide = arrCities.indexOf(arrCities[city]) + 1;
+	const firstLetter = arrCitiesDivided[city][0].charAt(0);
+
+	const $slide =
+		$(`<div data-target="${numSlide}" class="slide slide--${numSlide}">
 							<div class="slide__darkbg slide--${numSlide}__darkbg"></div>
 							<div class="slide__text-wrapper slide--${numSlide}__text-wrapper"></div>
 						</div>`);
 
-		const letter = 
-					$(`<div class="slide__letter slide--${numSlide}__letter">
+	const letter =
+		$(`<div class="slide__letter slide--${numSlide}__letter">
 							${firstLetter}
 						</div>`);
 
-		for (let i = 0, length = arrCitiesDivided[city].length; i < length; i++) {
-			const text = 
-						$(`<div class="slide__text slide__text--${i + 1}">
+	for (let i = 0, length = arrCitiesDivided[city].length; i < length; i++) {
+		const text =
+			$(`<div class="slide__text slide__text--${i + 1}">
 								${arrCitiesDivided[city][i]}
 							</div>`);
-			frag1.append(text);
-		}
-
-		const navSlide = $(`<li data-target="${numSlide}" class="nav__slide nav__slide--${numSlide}"></li>`);
-		frag2.append(navSlide);
-		$nav.append(frag2);
-
-		$slide.find(`.slide--${numSlide}__text-wrapper`).append(letter).append(frag1);
-		$slider.append($slide);
-
-		if (arrCities[city].length <= 4) {
-			$('.slide--'+ numSlide).find('.slide__text').css("font-size", "12vw");
-		}
-	};
-
-	for (let i = 0, length = numOfCities; i < length; i++) {
-		generateSlide(i);
+		frag1.append(text);
 	}
 
-	$('.nav__slide--1').addClass('nav-active');
+	const navSlide = $(`<li data-target="${numSlide}" class="nav__slide nav__slide--${numSlide}"></li>`);
+	frag2.append(navSlide);
+	$nav.append(frag2);
 
-	// Navigation
-	function bullets(dir) {
-		$('.nav__slide--' + curSlide).removeClass('nav-active');
-		$('.nav__slide--' + dir).addClass('nav-active');
+	$slide.find(`.slide--${numSlide}__text-wrapper`).append(letter).append(frag1);
+	$slider.append($slide);
+
+	if (arrCities[city].length <= 4) {
+		$('.slide--' + numSlide).find('.slide__text').css("font-size", "12vw");
 	}
-	
-	function timeout() {
-		animation = false;
-	}
-	
-	function pagination(direction) {
-		animation = true;
-		diff = 0;
-		$slider.addClass('animation');
+};
+
+for (let i = 0, length = numOfCities; i < length; i++) {
+	generateSlide(i);
+}
+
+$('.nav__slide--1').addClass('nav-active');
+
+// Navigation
+function bullets(dir) {
+	$('.nav__slide--' + curSlide).removeClass('nav-active');
+	$('.nav__slide--' + dir).addClass('nav-active');
+}
+
+function timeout() {
+	animation = false;
+}
+
+function pagination(direction) {
+	animation = true;
+	diff = 0;
+	$slider.addClass('animation');
+	$slider.css({
+		'transform': 'translate3d(-' + ((curSlide - direction) * 100) + '%, 0, 0)'
+	});
+
+	$slider.find('.slide__darkbg').css({
+		'transform': 'translate3d(' + ((curSlide - direction) * 50) + '%, 0, 0)'
+	});
+
+	$slider.find('.slide__letter').css({
+		'transform': 'translate3d(0, 0, 0)',
+	});
+
+	$slider.find('.slide__text').css({
+		'transform': 'translate3d(0, 0, 0)'
+	});
+}
+
+function navigateRight() {
+	if (!autoScrollVar) return;
+	if (curSlide >= numOfCities) return;
+	pagination(0);
+	setTimeout(timeout, animSpd);
+	bullets(curSlide + 1);
+	curSlide++;
+}
+
+function navigateLeft() {
+	if (curSlide <= 1) return;
+	pagination(2);
+	setTimeout(timeout, animSpd);
+	bullets(curSlide - 1);
+	curSlide--;
+}
+
+function toDefault() {
+	pagination(1);
+	setTimeout(timeout, animSpd);
+}
+
+// Events
+$(document).on('mousedown touchstart', '.slide', function (e) {
+	if (animation) return;
+	let target = +$(this).attr('data-target');
+	let startX = e.pageX || e.originalEvent.touches[0].pageX;
+	$slider.removeClass('animation');
+
+	$(document).on('mousemove touchmove', function (e) {
+		let x = e.pageX || e.originalEvent.touches[0].pageX;
+		diff = startX - x;
+		if (target === 1 && diff < 0 || target === numOfCities && diff > 0) return;
+
 		$slider.css({
-			'transform': 'translate3d(-' + ((curSlide - direction) * 100) + '%, 0, 0)'
+			'transform': 'translate3d(-' + ((curSlide - 1) * 100 + (diff / 30)) + '%, 0, 0)'
 		});
-		
-		$slider.find('.slide__darkbg').css({
-				'transform': 'translate3d(' + ((curSlide - direction) * 50) + '%, 0, 0)'
-		});
-		
-		$slider.find('.slide__letter').css({
-				'transform': 'translate3d(0, 0, 0)',
-		});
-		
-		$slider.find('.slide__text').css({
-			'transform': 'translate3d(0, 0, 0)'
-		});
-	}
-	
-	function navigateRight() {
-		if (!autoScrollVar) return;
-		if (curSlide >= numOfCities) return;
-		pagination(0);
-		setTimeout(timeout, animSpd);
-		bullets(curSlide + 1);
-		curSlide++;
-	}
-	
-	function navigateLeft() {
-		if (curSlide <= 1) return;
-		pagination(2);
-		setTimeout(timeout, animSpd);
-		bullets(curSlide - 1);
-		curSlide--;
-	}
 
-	function toDefault() {
-		pagination(1);
-		setTimeout(timeout, animSpd);
-	}
-	
-	// Events
-	$(document).on('mousedown touchstart', '.slide', function(e) {
-		if (animation) return;
-		let target = +$(this).attr('data-target');
-		let startX = e.pageX || e.originalEvent.touches[0].pageX;
-		$slider.removeClass('animation');
-		
-		$(document).on('mousemove touchmove', function(e) {
-			let x = e.pageX || e.originalEvent.touches[0].pageX;
-			diff = startX - x;
-			if (target === 1 && diff < 0 || target === numOfCities && diff > 0) return;
-			
-			$slider.css({
-				'transform': 'translate3d(-' + ((curSlide - 1) * 100 + (diff / 30)) + '%, 0, 0)'
-			});
-			
-			$slider.find('.slide__darkbg').css({
-				'transform': 'translate3d(' + ((curSlide - 1) * 50 + (diff / 60)) + '%, 0, 0)'
-			});
-			
-			$slider.find('.slide__letter').css({
-				'transform': 'translate3d(' +  (diff / 60) + 'vw, 0, 0)',
-			});
-			
-			$slider.find('.slide__text').css({
-				'transform': 'translate3d(' + (diff / 15) + 'px, 0, 0)'
-			});
-		})	
+		$slider.find('.slide__darkbg').css({
+			'transform': 'translate3d(' + ((curSlide - 1) * 50 + (diff / 60)) + '%, 0, 0)'
+		});
+
+		$slider.find('.slide__letter').css({
+			'transform': 'translate3d(' + (diff / 60) + 'vw, 0, 0)',
+		});
+
+		$slider.find('.slide__text').css({
+			'transform': 'translate3d(' + (diff / 15) + 'px, 0, 0)'
+		});
 	})
-	
-	$(document).on('mouseup touchend', function(e) {
-		$(document).off('mousemove touchmove');
-		
-		if (animation) return;
-		
-		if (diff >= distOfLetGo) {
-			navigateRight();
-		} else if (diff <= -distOfLetGo) {
-			navigateLeft();
-		} else {
-			toDefault();
-		}
-	});
-	
-	$(document).on('click', '.nav__slide:not(.nav-active)', function() {
-		let target = +$(this).attr('data-target');
-		bullets(target);
-		curSlide = target;
-		pagination(1);
-	});	
-	
-	$(document).on('click', '.side-nav', function() {
-		let target = $(this).attr('data-target');
-		
-		if (target === 'right') navigateRight();
-		if (target === 'left') navigateLeft();
-	});
-	
-	$(document).on('keydown', function(e) {
-		if (e.which === 39) navigateRight();
-		if (e.which === 37) navigateLeft();
-	});
-	
-	$(document).on('mousewheel DOMMouseScroll', function(e) {
-		if (animation) return;
-    let delta = e.originalEvent.wheelDelta;
-		
-    if (delta > 0 || e.originalEvent.detail < 0) navigateLeft();
-	 	if (delta < 0 || e.originalEvent.detail > 0) navigateRight();
-  });
+})
+
+$(document).on('mouseup touchend', function (e) {
+	$(document).off('mousemove touchmove');
+
+	if (animation) return;
+
+	if (diff >= distOfLetGo) {
+		navigateRight();
+	} else if (diff <= -distOfLetGo) {
+		navigateLeft();
+	} else {
+		toDefault();
+	}
+});
+
+$(document).on('click', '.nav__slide:not(.nav-active)', function () {
+	let target = +$(this).attr('data-target');
+	bullets(target);
+	curSlide = target;
+	pagination(1);
+});
+
+$(document).on('click', '.side-nav', function () {
+	let target = $(this).attr('data-target');
+
+	if (target === 'right') navigateRight();
+	if (target === 'left') navigateLeft();
+});
+
+$(document).on('keydown', function (e) {
+	if (e.which === 39) navigateRight();
+	if (e.which === 37) navigateLeft();
+});
+
+$(document).on('mousewheel DOMMouseScroll', function (e) {
+	if (animation) return;
+	let delta = e.originalEvent.wheelDelta;
+
+	if (delta > 0 || e.originalEvent.detail < 0) navigateLeft();
+	if (delta < 0 || e.originalEvent.detail > 0) navigateRight();
+});
 
 // var allButtons = document.querySelectorAll('.icon_head');
 // console.log("Found", allButtons.length, "div which class starts with “button”.");
@@ -332,24 +332,72 @@ $(document).ready(function () {
 
 
 
-$(document).ready(function() { 
+$(document).ready(function () {
 
 	// tabs
-	(function ($) { 
+	(function ($) {
 		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-		
-		$('.tab ul.tabs li a').click(function (g) { 
-			var tab = $(this).closest('.tab'), 
+
+		$('.tab ul.tabs li a').click(function (g) {
+			var tab = $(this).closest('.tab'),
 				index = $(this).closest('li').index();
 			tab.find('ul.tabs > li').removeClass('current');
 			$(this).closest('li').addClass('current');
 			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp()
 			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
 			g.preventDefault();
-		} );
+		});
 	})(jQuery);
 
 });
 
 
 
+// slick slider at product details
+
+$('.slider-for').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-nav',
+	speed: 500
+});
+$('.slider-nav').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	dots: false,
+	centerMode: true,
+	focusOnSelect: true,
+	speed: 500
+});
+
+//   related products slider
+$('.multiple-items').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	speed: 300,
+	arrows: true,
+	autoplay: true,
+	dots: true,
+	responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 1,
+		  }
+		},
+		{
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1
+		  }
+		},
+	
+
+	  ]
+});
